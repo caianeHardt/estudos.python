48ccad89550226bf13ddcb8c77c73c39a3050983
Caiane Hardt <caianehardt@gmail.com>
2021-10-14 18:11:22 -0300

    add material de cursos em python

---
+++ PythonExercicios/ex002.py
@@ -0,0 +1,14 @@
+nome=input('Qual é o seu nome?')
+print("Olá,"+nome+" prazer em te conhecer!")
+
+#Existe mais esses dois modos:
+#nome=input('Qual é o seu nome?')
+#print(“Prazer em te conhecer, ”,nome)
+
+#nome=input('Qual é o seu nome?')
+#print("Prazer em te conhecer, {}!" .format(nome))
+
+
+
+
+
